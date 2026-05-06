83477ec7472180158f77aa5e8deb2aca61207eab
My laptop <samiraomar.xx@gmail.com>
2026-05-06 11:59:25 +0300

    modified app/config.php

--- app/config.php
+++ app/config.php
@@ -1,32 +1,33 @@
 <?php
 
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-// Load .env from project root
+/* =========================
+   LOAD .ENV
+========================= */
 $dotenv = Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-/* -------------------------
+/* =========================
    APP SETTINGS
---------------------------*/
+========================= */
 define('APP_NAME', $_ENV['APP_NAME']);
 define('APP_ENV', $_ENV['APP_ENV']);
 define('APP_DEBUG', filter_var($_ENV['APP_DEBUG'], FILTER_VALIDATE_BOOLEAN));
 
-/* -------------------------
+/* =========================
    DATABASE SETTINGS
---------------------------*/
+========================= */
 define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_NAME', $_ENV['DB_NAME']);
 define('DB_USER', $_ENV['DB_USER']);
 define('DB_PASS', $_ENV['DB_PASS']);
 
-/* -------------------------
+/* =========================
    ERROR HANDLING
---------------------------*/
+========================= */
 if (APP_DEBUG) {
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
